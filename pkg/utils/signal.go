cbd7b3bdd9fbab69b761ab83e1ce228a6e9aa9c9
xueZhixin <xuezhixin@uniontech.com>
2025-09-04 16:40:30 +0800

    增加utils调用模块

---
+++ pkg/utils/signal.go
@@ -0,0 +1,24 @@
+// SPDX-FileCopyrightText: 2025 UnionTech Software Technology Co., Ltd.
+// SPDX-License-Identifier: MIT
+package utils
+
+import (
+	"github.com/sirupsen/logrus"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+)
+
+func HandleSignals(function func()) {
+	var callback sync.Once
+	sigc := make(chan os.Signal, 1)
+	defer close(sigc)
+	signal.Notify(sigc,
+		syscall.SIGINT,
+		syscall.SIGTERM)
+
+	sig := <-sigc
+	logrus.Infof("service received signal: %v", sig)
+	callback.Do(function)
+}
